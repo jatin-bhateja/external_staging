f913a4fefc5ad8164fd58220d7d258451d1093cb
Jatin Bhateja <jatin.bhateja@intel.com>
2024-07-15 17:15:17 +0530

    Incremental wip items

--- NewOperationSamples/cross-vector-permute/selectFromTwoVectorsInt.java
+++ NewOperationSamples/cross-vector-permute/selectFromTwoVectorsInt.java
@@ -3,37 +3,36 @@ import jdk.incubator.vector.*;
 import java.util.Arrays;
 import java.util.stream.IntStream;
 
-public class selectFromTwoVectors {
-    public static final VectorSpecies<Float> FSP = FloatVector.SPECIES_256;
+public class selectFromTwoVectorsInt {
     public static final VectorSpecies<Integer> ISP = IntVector.SPECIES_256;
 
-    public static void micro1(float [] res, float [] src1, float [] src2, float [] idx) {
-        for (int i = 0; i < FSP.loopBound(res.length); i += FSP.length()) {
-            FloatVector vidx = FloatVector.fromArray(FSP, idx, i);
-            if (vidx.convert(VectorOperators.F2I, 0).compare(VectorOperators.UNSIGNED_GE, FSP.length() * 2).anyTrue()) {
-                throw new IndexOutOfBoundsException("OOB" + "[" + i + "," + (i + FSP.length()) + ")");
+    public static void micro1(int [] res, int [] src1, int [] src2, int [] idx) {
+        for (int i = 0; i < ISP.loopBound(res.length); i += ISP.length()) {
+            IntVector vidx = IntVector.fromArray(ISP, idx, i);
+            if (vidx.compare(VectorOperators.UNSIGNED_GE, ISP.length() * 2).anyTrue()) {
+                throw new IndexOutOfBoundsException("OOB" + "[" + i + "," + (i + ISP.length()) + ")");
             }
-            FloatVector.fromArray(FSP, src1, i)
-                       .rearrange(vidx.toShuffle(), FloatVector.fromArray(FSP, src2, i))
+            IntVector.fromArray(ISP, src1, i)
+                       .rearrange(vidx.toShuffle(), IntVector.fromArray(ISP, src2, i))
                        .intoArray(res, i);
         }
     }
 
-    public static void micro2(float [] res, float [] src1, float [] src2, float [] idx) {
-        for (int i = 0; i < FSP.loopBound(res.length); i += FSP.length()) {
-            FloatVector vidx = FloatVector.fromArray(FSP, idx, i);
-            FloatVector vsrc1 = FloatVector.fromArray(FSP, src1, i);
-            FloatVector vsrc2 = FloatVector.fromArray(FSP, src2, i);
+    public static void micro2(int [] res, int [] src1, int [] src2, int [] idx) {
+        for (int i = 0; i < ISP.loopBound(res.length); i += ISP.length()) {
+            IntVector vidx = IntVector.fromArray(ISP, idx, i);
+            IntVector vsrc1 = IntVector.fromArray(ISP, src1, i);
+            IntVector vsrc2 = IntVector.fromArray(ISP, src2, i);
             vidx.selectFrom(vsrc1, vsrc2)
                 .intoArray(res, i);
         }
     }
 
-    public static void micro3(float [] res, float [] src1, float [] src2, float [] idx) {
-        for (int i = 0; i < FSP.loopBound(res.length); i += FSP.length()) {
-            FloatVector vidx = FloatVector.fromArray(FSP, idx, i);
-            FloatVector vsrc1 = FloatVector.fromArray(FSP, src1, i);
-            FloatVector vsrc2 = FloatVector.fromArray(FSP, src2, i);
+    public static void micro3(int [] res, int [] src1, int [] src2, int [] idx) {
+        for (int i = 0; i < ISP.loopBound(res.length); i += ISP.length()) {
+            IntVector vidx = IntVector.fromArray(ISP, idx, i);
+            IntVector vsrc1 = IntVector.fromArray(ISP, src1, i);
+            IntVector vsrc2 = IntVector.fromArray(ISP, src2, i);
             vsrc1.rearrange(vidx.toShuffle(), vsrc2)
                 .intoArray(res, i);
         }
@@ -41,18 +40,16 @@ public class selectFromTwoVectors {
 
     public static void main(String [] args) {
        int algo = Integer.parseInt(args[0]);
-       float [] src1 = new float[4096];
-       float [] src2 = new float[4096];
-       float [] idx = new float[4096];
-       float [] idx_bs = new float[4096];
-       int FSP_LEN_M1 = FSP.length() - 1;
-       IntStream.range(0, 4096).forEach( i -> {src1[i] = (float)i;});
-       IntStream.range(0, 4096).forEach( i -> {src2[i] = (float)i + 4096.0f;});
-       IntStream.range(0, 4096).forEach( i -> {idx[i] = (i % 2 == 0) ? (float)(i & FSP_LEN_M1): (float)((i & FSP_LEN_M1) + FSP.length());});
+       int [] idx = new int[4096];
+       int [] idx_bs = new int[4096];
+       int ISP_LEN_M1 = ISP.length() - 1;
+       int [] src1 = IntStream.range(0, 4096).toArray();
+       int [] src2 = IntStream.range(4096, 4096*2).toArray();
+       IntStream.range(0, 4096).forEach( i -> {idx[i] = (i % 2 == 0) ? (int)(i & ISP_LEN_M1): (int)((i & ISP_LEN_M1) + ISP.length());});
        System.arraycopy(idx, 0, idx_bs, 0, idx.length);
    
        if (algo == 0 || algo == -1) {
-           float [] res = new float[4096];
+           int [] res = new int[4096];
            for (int i = 0; i < 100000 ; i++) {
                micro1(res, src1, src2, idx);
                System.arraycopy(idx_bs, 0, idx, 0, idx.length);
@@ -67,7 +64,7 @@ public class selectFromTwoVectors {
            System.out.println("[time micro1] " + (t2-t1) + " ms   [res] " + Arrays.toString(Arrays.copyOfRange(res, 0, 16)));
        }
        if (algo == 1 || algo == -1) {
-           float [] res = new float[4096];
+           int [] res = new int[4096];
            for (int i = 0; i < 100000 ; i++) {
                micro2(res, src1, src2, idx);
                System.arraycopy(idx_bs, 0, idx, 0, idx.length);
@@ -82,7 +79,7 @@ public class selectFromTwoVectors {
            System.out.println("[time micro2] " + (t2-t1) + " ms   [res] " + Arrays.toString(Arrays.copyOfRange(res, 0, 16)));
        }
        if (algo == 2 || algo == -1) {
-           float [] res = new float[4096];
+           int [] res = new int[4096];
            for (int i = 0; i < 100000 ; i++) {
                micro3(res, src1, src2, idx);
                System.arraycopy(idx_bs, 0, idx, 0, idx.length);
