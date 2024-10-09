c70a9d3e7bec9e5c9caaccf3ab36d581417dea8a
Jatin Bhateja <jatin.bhateja@intel.com>
2024-10-09 20:59:16 +0530

    Update

--- VectorizedAlgos/vectorized_scan.java
+++ VectorizedAlgos/vectorized_scan.java
@@ -19,32 +19,35 @@ public class vectorized_scan {
        // 16 elements = 16 Loads + 16 Stores + 16 Additions.
    }
 
-   public static void scan_parallel_algo1(float [] res, float [] src) {
-       // Perm Table:
-       // A B C D E F G H
-       //   A B C D E F G
-       //     A B C D E F
-       //       A B C D E
-       //         A B C D
-       //           A B C
-       //             A B
-       //               A
+   public static void simd_scan_algo1(float [] res, float [] src) {
+       // For purpose of illustration assuming 256 bit FloatVector input
+       // with eight vector lanes.
        //
-       //   - Algorithm:
-       //     INDEX =  IOTA
-       //     ONEV = BCAST(1)
-       //     DST = BCAST(0)
-       //     Loop_Over_Rest : (INCR=FSP.length())
-       //        SRC =  LoadVector SRC
-       //        TEMP = PERM SRC INDEX    | Alternative
-       //        INDEX = INDEX +  ONEV    | -> Use VECTOR EXPAND with progressively right shifting of mask
-       //        DST = ADD SRC DST (MASK) |
-       //        
-       //        Propagate the result of last vector lane across vector loop iterations.  
-       //        [ VRES1  ] [VRES2   ]  [VRES3 ]  [VRES4  ]
-       //                V   A       V   A     V   A
-       //                |___|       |___|     |___|
+       // Iter[0:VECLEN)   Input Vector      Expand mask      Output
+       // ------------------------------------------------------------------
+       //         0      A B C D E F G H    0x11111111     A B C D E F G H
+       //         1      A B C D E F G H    0x11111110       A B C D E F G
+       //         2      A B C D E F G H    0x11111100         A B C D E F
+       //         3      A B C D E F G H    0x11111000           A B C D E
+       //         4      A B C D E F G H    0x11110000             A B C D
+       //         5      A B C D E F G H    0x11100000               A B C
+       //         6      A B C D E F G H    0x11000000                 A B
+       //         7      A B C D E F G H    0x10000000                   A
+       //===================================================================
+       //  Add the expanded input vector produced after every iteration
+       //  to get final vector scan result.
+       //===================================================================                    
        //
+       // In order to perform the scan over arrays whose length is greater
+       // than the vector length, broadcast last lane of resultant vector of
+       // previous scan operation and add it to input vector of current scan
+       // operation before commencing scan iterations.
+       // 
+       //   [ SCAN1 ] [ SCAN2 ] [ SCAN3 ] [ SCAN4 ]
+       //          V   A     V   A     V   A
+       //          |___|     |___|     |___|
+       //
+
        FloatVector vres = FloatVector.broadcast(FSP, 0.0f);
        for (int i = 0; i < FSP.loopBound(res.length); i += FSP.length()) {
            long mask = (1L << FSP.length()) -1;
@@ -53,16 +56,31 @@ public class vectorized_scan {
               vres = vec1.expand(VectorMask.fromLong(FSP, mask))
                          .lanewise(VectorOperators.ADD, vres);
               mask <<= 1;
-                  
            }
            vres.intoArray(res, i);
            vres = FloatVector.broadcast(FSP, res[i + FSP.length() -1]);
        }
    }
 
+   public Vector<E> scanLanes_Paul(VectorOperators.Associative op, VectorMask<E> m, ETYPE leftInput) {
+      ETYPE[] a = this.toArray();
+      var partial = leftInput;
+      var noLanesSet = VectorMask.fromLong(species(), 0);
+      var leftId = this.reduceLanes(op, noLanesSet);
+      //var leftId = VectorOperations.leftIdentity(op); // hypothetical API
+      for (int i = 0; i < a.length; i++) {
+        if (m.laneIsSet(i)) partial = leftId;
+        var ai = a[i];
+        ai = this.broadcast(partial).lanewise(op, this.broadcast(ai)).lane(0);
+        //ai = VectorOperations.execute(op, partial, ai); // hypothetical API
+        a[i] = ai;
+        partial = ai;
+      }
+      return fromArray(a);
+    }
 
    // TBD: https://www.intel.com/content/www/us/en/developer/articles/technical/optimize-scan-operations-explicit-vectorization.html
-   public static void scan_parallel_algo2(float [] res, float [] src) {
+   public static void simd_scan_algo2(float [] res, float [] src) {
 
    }
 
@@ -89,11 +107,11 @@ public class vectorized_scan {
 
        if (algo == 1 || algo == -1) {
            for (int i = 0; i < 100000; i++) {
-               scan_parallel_algo1(res, src);
+               simd_scan_algo1(res, src);
            }
            long t1 = System.currentTimeMillis();
            for (int i = 0; i < 100000; i++) {
-               scan_parallel_algo1(res, src);
+               simd_scan_algo1(res, src);
            }
            long t2 = System.currentTimeMillis();
            System.out.println("[vector time] " + (t2-t1) + " ms  [res] " + Arrays.toString(Arrays.copyOfRange(res, 0, 16)));
