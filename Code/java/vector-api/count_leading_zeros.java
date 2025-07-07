2ce38317ca2438abcae579694b6b605fc12ed403
Jatin Bhateja <jatin.bhateja@intel.com>
2025-07-07 12:17:41 +0530

    emperical proof

---
+++ Code/java/vector-api/count_leading_zeros.java
@@ -0,0 +1,49 @@
+
+import jdk.incubator.vector.*;
+import java.util.Arrays;
+
+public class count_leading_zeros {
+   public static VectorSpecies<Integer> ISP = IntVector.SPECIES_PREFERRED;
+
+   public static void micro(int [] dst, int [] src) {
+       IntVector.fromArray(ISP, src, 0)
+                .lanewise(VectorOperators.LEADING_ZEROS_COUNT)
+                .intoArray(dst, 0);
+   }
+
+   public static void main(String [] args) {
+       int [] src = {
+           67108863, 67108863, 67108863, 67108863, 
+           67108863, 67108863, 67108863, 67108863, 
+           67108863, 67108863, 67108863, 67108863, 
+           67108863, 67108863, 67108863, 67108863, 
+       };
+       int [] dst = new int[16];
+       for (int i = 0; i < 100000; i++) {
+           micro(dst, src);
+       }
+       System.out.println("[res] " + Arrays.toString(dst));
+   }
+}
+
+/*
+   Emperically determined expression ~(src >> 1) & src to turn off all bit after leftmost set bits.
+
+        jshell> for (int i = 0; i < Integer.MAX_VALUE; i++) {
+           ...>     int new_i = ~(i >>> 1) & i;
+           ...>     if (new_i != i) {
+           ...>         System.out.println(new_i + " != " + i); if (first_n == 0) break; else first_n--;
+           ...>     }
+           ...> }
+        2 != 3
+        4 != 6
+        4 != 7
+        10 != 11
+        8 != 12
+        9 != 13
+        8 != 14
+        8 != 15
+        18 != 19
+        20 != 22
+        20 != 23
+*/
