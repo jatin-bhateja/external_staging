ab893843aa449918c7817586d106d51b9390c832
Jatin Bhateja <jatin.bhateja@gmail.com>
2025-09-25 01:30:19 +0000

    Update makefiles

--- Valhalla/partial_escape_analysis_graal_c2_valhalla/JDK-8366137/run.sh
+++ Valhalla/partial_escape_analysis_graal_c2_valhalla/JDK-8366137/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-for i in $(ls -d */); do cd ${i}; echo ${i}; make all | grep -e "\[RUN\]" -e "\[time\]" 2> /dev/null > PERF.txt; cd ..; done
+for i in $(ls -d */); do cd ${i}; echo ${i}; make all | grep -e "\[RUN\]" -e "\[.*time.*\]" 2> /dev/null > PERF.txt; cd ..; done
